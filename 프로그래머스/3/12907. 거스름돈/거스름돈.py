969eaef8fa42838933e299a983918c38d55b024f
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-08-26 22:25:06 +0900

    [level 3] Title: 거스름돈, Time: 295.36 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,13 @@
+def solution(n, money):
+    answer = 0
+    
+    money = sorted(money)
+    dp = [0] * (n+1)
+    
+    dp[0] = 1
+    
+    for m in money:
+        for i in range(m, n+1):
+            dp[i] += dp[i-m]
+
+    return dp[-1] % 1000000007
